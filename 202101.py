0d04585fd7ebaf2f6740b40c8bbb17fd6570ee84
GLgele <60538556+GLgele@users.noreply.github.com>
2021-09-12 16:42:04 +0800

    Update 202101.py

--- 202101.py
+++ 202101.py
@@ -1,1 +1,10 @@
 #等差数列
+start = float(input("Input start\n"))
+end = float(input("Input end\n"))
+step = float(input("Input step\n"))
+i = start
+sum = 0.0
+while i <= end:
+    sum += i
+    i += step
+print(sum)
